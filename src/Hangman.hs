3a5ea95761b832c586561839280f972ee05418ea
Alexander Kobjolke <alex@jakalx.net>
2021-04-13 19:49:18 +0200

    implement simple hangman game

--- src/Hangman.hs
+++ src/Hangman.hs
@@ -1,20 +1,60 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Hangman (hangman) where
+module Hangman
+   -- (hangman)
+    where
 
-data Game = Game { secret :: String }
+import Data.List (intersperse, insert)
+import Data.Maybe (fromMaybe, catMaybes)
+import Data.Char (toLower)
+import System.IO (hFlush, stdout)
+import System.Random (randomRIO)
 
-newGame :: String -> Game
-newGame w = Game { secret = w }
+data GameState = GameState { gsSecret :: String,
+  gsGuessed :: String,
+  gsSolution :: [Maybe Char]
+} deriving Show
 
-move :: Game -> Char -> Game
-move g@Game{..} guess = undefined
+newGame :: String -> GameState
+newGame w = GameState { gsSecret = w, gsGuessed = [], gsSolution = map (const Nothing) w }
 
+handleGuess :: Char -> Char -> Maybe Char -> Maybe Char
+handleGuess guess secret prev
+  | toLower guess == toLower secret = Just secret
+  | otherwise = prev
+
+move :: GameState -> Char -> GameState
+move g@GameState{..} guess = g { gsGuessed = insert guess gsGuessed, gsSolution = newSolution }
+  where newSolution = zipWith (handleGuess guess) gsSecret gsSolution
 
 loadWords :: FilePath -> IO [String]
 loadWords f = lines <$> readFile f
 
+play :: GameState -> IO ()
+play game@GameState{..} =
+    if catMaybes gsSolution == gsSecret
+      then putStrLn $ render game
+      else do
+        putStrLn $ render game
+        putStr "enter guess: "
+        hFlush stdout
+        guess <- getChar
+        putStrLn ""
+        play $ move game guess
+
+chooseRandomWord :: [String] -> IO (Maybe String)
+chooseRandomWord [] = return Nothing
+chooseRandomWord xs = do
+  index <- randomRIO (0, length xs - 1)
+  return $ Just (xs !! index)
+
 hangman :: IO ()
 hangman = do
-    words <- loadWords "data/words.txt"
-    print words
+    w <- loadWords "data/words.txt" >>= chooseRandomWord
+    case w of
+       Nothing -> return ()
+       Just w -> play $ newGame w
+
+render :: GameState -> String
+render GameState{..} = intersperse ' ' w
+  where w = map (fromMaybe '_') gsSolution
